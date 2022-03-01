0595b72df20b59fdf12179d4969076db3a0a360e
Stephen Eneji <enejistephen01@gmail.com>
2022-03-01 22:10:21 +0100

    Matrispy Update 1st March 2022
    Latest update for Matrix program :)

--- main.py
+++ main.py
@@ -24,12 +24,15 @@
 
 print("WELCOME TO MATRIPSY")
 
-selectioninput = input(f"Input {1} for 2x2 matrix or {2} for 3x3 matrix: ")
-#Script for 2x2 matrix
+selectioninput = input(f"Input 1 for 2x2 matrix or 2 for 3x3 matrix: ")
 
-def logicfor2x2(self):
+
+#Script for 2x2 matrix
+def logicfor2x2():
 # The Location of the characters of the matrix using rows and column #Mat1
 # Matrix 1
+
+    print("Data for Matrix 1")
     m1_row1 = input("Row 1, Col 1: ")
     m1_row2 = input("Row 1, Col 2: ")
     m1_col1 = input("Row 2, Col 1: ")
@@ -37,52 +40,47 @@ def logicfor2x2(self):
 
     # The Location of the characters of the matrix using rows and columns #Mat2
     # Matrix 2
+    print("Data for Matrix 2")
     m2_row1 = input("Row 1, Col 1: ")
     m2_row2 = input("Row 1, Col 2: ")
     m2_col1 = input("Row 2, Col 1: ")
     m2_col2 = input("Row 2, Col 2: ")
 
     # Logic
-    def input_validation():
+    # MATRIX 1
+    m1_row1_num = float(m1_row1)
+    m1_row2_num = float(m1_row2)
+    m1_col1_num = float(m1_col1)
+    m1_col2_num = float(m1_col2)
 
-        # MATRIX 1
-        m1_row1_num = float(m1_row1)
-        m1_row2_num = float(m1_row2)
-        m1_col1_num = float(m1_col1)
-        m1_col2_num = float(m1_col2)
+    # MATRIX 2
+    m2_row1_num = float(m2_row1)
+    m2_row2_num = float(m2_row2)
+    m2_col1_num = float(m2_col1)
+    m2_col2_num = float(m2_col2)
 
-        # MATRIX 2
-        m2_row1_num = float(m2_row1)
-        m2_row2_num = float(m2_row2)
-        m2_col1_num = float(m2_col1)
-        m2_col2_num = float(m2_col2)
+    if m1_row1.isdigit():
+        print(m1_row1_num + m2_row1_num)
 
-        if m1_row1.isdigit():
-            print(m1_row1_num + m2_row1_num)
+    if m1_row2.isdigit():
+        print(m1_row2_num + m2_row2_num)
 
-        if m1_row2.isdigit():
-            print(m1_row2_num + m2_row2_num)
+    if m1_col1.isdigit():
+        print(m1_col1_num + m2_col1_num)
 
-        if m1_col1.isdigit():
-            print(m1_col1_num + m2_col1_num)
-
-        if m2_col2.isdigit():
-            print(m1_col2_num + m2_col2_num)
-        else:
-            print("Enter 4 number")
-
-    input_validation()
-
-    logicfor2x2(1)
+    if m2_col2.isdigit():
+        print(m1_col2_num + m2_col2_num)
+    else:
+        print("Enter valid number")
 
 
 
 #Script for 3x3 matrix
-def logicfor3x3(self):
+def logicfor3x3():
     # The Location of the characters of the matrix using rows and column #Mat1
-
     # Matrix 1
     #Row1
+    print("Data for Matrix 1")
     m1_r1c1 = input("Row 1, Col 1: ")
     m1_r1c2 = input("Row 1, Col 2: ")
     m1_r1c3 = input("Row 1, Col 3: ")
@@ -99,6 +97,7 @@ def logicfor3x3(self):
 
 
     # The Location of the characters of the matrix using rows and columns #Mat2
+    print("Data for Matrix 2")
     # Matrix 2
     # Row1
     m2_r1c1 = input("Row 1, Col 1: ")
@@ -116,73 +115,73 @@ def logicfor3x3(self):
     m2_r3c3 = input("Row 3, Col 3: ")
 
     # Logic
-    def input_validation_3():
-        # MATRIX 1
-        m1_r1c1_num = float(m1_r1c1)
-        m1_r1c2_num = float(m1_r1c2)
-        m1_r1c3_num = float(m1_r1c3)
 
-        m1_r2c1_num = float(m1_r2c1)
-        m1_r2c2_num = float(m1_r2c2)
-        m1_r2c3_num = float(m1_r2c3)
+    # MATRIX 1
+    m1_r1c1_num = float(m1_r1c1)
+    m1_r1c2_num = float(m1_r1c2)
+    m1_r1c3_num = float(m1_r1c3)
 
-        m1_r3c1_num = float(m1_r3c1)
-        m1_r3c2_num = float(m1_r3c2)
-        m1_r3c3_num = float(m1_r3c3)
+    m1_r2c1_num = float(m1_r2c1)
+    m1_r2c2_num = float(m1_r2c2)
+    m1_r2c3_num = float(m1_r2c3)
 
-        # MATRIX 2
-        m2_r1c1_num = float(m2_r1c1)
-        m2_r1c2_num = float(m2_r1c2)
-        m2_r1c3_num = float(m2_r1c3)
+    m1_r3c1_num = float(m1_r3c1)
+    m1_r3c2_num = float(m1_r3c2)
+    m1_r3c3_num = float(m1_r3c3)
 
-        m2_r2c1_num = float(m2_r2c1)
-        m2_r2c2_num = float(m2_r2c2)
-        m2_r2c3_num = float(m2_r2c3)
+    # MATRIX 2
+    m2_r1c1_num = float(m2_r1c1)
+    m2_r1c2_num = float(m2_r1c2)
+    m2_r1c3_num = float(m2_r1c3)
 
-        m2_r3c1_num = float(m2_r3c1)
-        m2_r3c2_num = float(m2_r3c2)
-        m2_r3c3_num = float(m2_r3c3)
+    m2_r2c1_num = float(m2_r2c1)
+    m2_r2c2_num = float(m2_r2c2)
+    m2_r2c3_num = float(m2_r2c3)
 
-        if m1_r1c1.isdigit():
-            print(m1_r1c1_num + m2_r1c1_num)
+    m2_r3c1_num = float(m2_r3c1)
+    m2_r3c2_num = float(m2_r3c2)
+    m2_r3c3_num = float(m2_r3c3)
 
-        if m1_r1c2.isdigit():
-            print(m1_r1c2_num + m2_r1c2_num)
+    if m1_r1c1.isdigit():
+        print(m1_r1c1_num + m2_r1c1_num)
 
-        if m1_r1c3.isdigit():
-            print(m1_r1c3_num + m2_r1c3_num)
+    if m1_r1c2.isdigit():
+        print(m1_r1c2_num + m2_r1c2_num)
 
-        #Row2
-        if m1_r2c1.isdigit():
-            print(m1_r2c1_num + m2_r2c1_num)
+    if m1_r1c3.isdigit():
+        print(m1_r1c3_num + m2_r1c3_num)
 
-        if m1_r2c2.isdigit():
-            print(m1_r2c1_num + m2_r2c1_num)
+    #Row2
+    if m1_r2c1.isdigit():
+        print(m1_r2c1_num + m2_r2c1_num)
 
-        if m1_r2c3.isdigit():
-            print(m1_r2c3_num + m2_r2c3_num)
+    if m1_r2c2.isdigit():
+        print(m1_r2c1_num + m2_r2c1_num)
 
-        #Row3
-        if m1_r3c1.isdigit():
-            print(m1_r3c1_num + m2_r3c1_num)
+    if m1_r2c3.isdigit():
+        print(m1_r2c3_num + m2_r2c3_num)
 
-        if m1_r3c2.isdigit():
-            print(m1_r3c1_num + m2_r3c1_num)
+    #Row3
+    if m1_r3c1.isdigit():
+        print(m1_r3c1_num + m2_r3c1_num)
 
-        if m1_r3c3.isdigit():
-            print(m1_r3c3_num + m2_r3c3_num)
+    if m1_r3c2.isdigit():
+        print(m1_r3c1_num + m2_r3c1_num)
 
-        else:
-            print("Enter 6 number")
+    if m1_r3c3.isdigit():
+        print(m1_r3c3_num + m2_r3c3_num)
 
-    input_validation_3()
-logicfor3x3(2)
+    else:
+        print("Enter a valid number")
 
 
-def matrixtypeselection():
-    if selectioninput == 1:
-        print(logicfor2x2)
-    elif selectioninput == 2:
-        print(logicfor3x3)
-
-matrixtypeselection()
+#Logic to get user input and run either code 1 or two based on it
+int_selectioninput = int(selectioninput)
+def conditionsfortype():
+    if int_selectioninput <= 1:
+        return logicfor2x2()
+    elif int_selectioninput >= 2:
+        return logicfor3x3()
+    else:
+        print("Input 1 or 2")
+conditionsfortype()
